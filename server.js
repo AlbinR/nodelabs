303586be2f5a3b76ff01e71416e7798be1490bab
AlbinR <albin@rybergs.net>
2021-05-22 17:25:21 +0200

    added one random function

--- server.js
+++ server.js
@@ -2,12 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Serve static files in public
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
+// Random function
+app.get("/api/random", function (req, res) {
+  const random = Math.floor(Math.random() * 1023);
+  res.json({ number: random });
 });
 
+// Custom random function
+
+// App is listening to the webserver
 app.listen(port, function () {
-  console.log("Example app listening on port 3000!");
+  console.log("server listening to port", port);
 });
+
+/* 
+Sources: 
+https://expressjs.com/
+*/
