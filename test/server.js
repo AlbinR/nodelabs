961dbe4340612bd4362e0e7742068e74fa79d8b9
AlbinR <albin@rybergs.net>
2021-05-23 20:29:26 +0200

    little fix

--- test/server.js
+++ test/server.js
@@ -6,9 +6,8 @@ describe("random", function () {
 
   it("Should show an integer 0 - 1023", function (done) {
     request(url, function (error, response, body) {
-        expect(JSON.parse(response.body).number).to.be.within(0, 1023);
-        done();
-      });
+      expect(JSON.parse(response.body).number).to.be.within(0, 1023);
+      done();
     });
   });
 });
